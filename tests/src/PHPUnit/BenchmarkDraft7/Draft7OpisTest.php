7e93c5464ec223be24aa7b5ad335add2d029d5df
sorinsarca <sarca_sorin@hotmail.com>
2018-05-23 18:15:30 +0300

    Updated opis test classes

--- tests/src/PHPUnit/BenchmarkDraft7/Draft7OpisTest.php
+++ tests/src/PHPUnit/BenchmarkDraft7/Draft7OpisTest.php
@@ -2,6 +2,7 @@
 
 namespace Swaggest\JsonSchemaBench\Tests\PHPUnit\BenchmarkDraft7;
 
+use Opis\JsonSchema\Exception\AbstractSchemaException;
 use Opis\JsonSchema\Loaders\Memory;
 use Opis\JsonSchema\Schema;
 use Opis\JsonSchema\Validator;
@@ -47,17 +48,11 @@ class Draft7OpisTest extends Draft7Test
         return $schemaStorage;
     }
 
-    /** @var \SplObjectStorage */
-    private $schemasCache;
+    /** @var Schema */
+    private $schemaCache;
 
     /** @var Validator */
-    private $validator;
-
-    protected function setUp()
-    {
-        $this->schemasCache = new \SplObjectStorage();
-        $this->validator = new Validator(null, self::getLoader());
-    }
+    private $validator = null;
 
     /**
      * @param $schemaData
@@ -69,19 +64,22 @@ class Draft7OpisTest extends Draft7Test
      */
     protected function runSpecTest($schemaData, $data, $isValid, $name, $version)
     {
-        if (false && is_object($schemaData)) {
-            if ($this->schemasCache->contains($schemaData)) {
-                $schema = $this->schemasCache->offsetGet($schemaData);
-            } else {
-                $schema = new Schema($schemaData);
-                $this->schemasCache->attach($schemaData, $schema);
-            }
-        } else {
-            $schema = new Schema($schemaData);
-        }
-        $result = $this->validator->schemaValidation($data, $schema);
+        if ($this->validator === null) {
+            $this->validator = (new Validator())
+                ->varsSupport(false)
+                ->filtersSupport(false)
+                ->mapSupport(false)
+                ->setLoader(self::getLoader());
 
-        $actualValid = $result->isValid();
+            $this->schemaCache = new Schema($schemaData);
+        }
+
+        try {
+            $actualValid = $this->validator->schemaValidation($data, $this->schemaCache)->isValid();
+        }
+        catch (AbstractSchemaException $e) {
+            $actualValid = false;
+        }
 
         $this->assertSame($isValid, $actualValid, "Test: $name");
     }
